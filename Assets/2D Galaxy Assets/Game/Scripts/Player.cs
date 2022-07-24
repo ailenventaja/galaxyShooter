a1d82e0520bf12a523c315b6ebc040619cb7c1e0
AILENVENTAJA1\Admin <ailen.ventaja@neoris.com>
2022-07-24 17:43:14 -0300

    laser shoot settings

--- Assets/2D Galaxy Assets/Game/Scripts/Player.cs
+++ Assets/2D Galaxy Assets/Game/Scripts/Player.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField]
+    private GameObject _laserPrefab;
+    private float _positionX;
+    private float _positionY;
+    private float _positionZ;
+    [SerializeField]
+    private float _fireRate = 0.25f;
+    private float _lastShoot = 0.0f;
+
+    [SerializeField]
     private float speed = 5.0f;
 
     // Start is called before the first frame update
@@ -17,20 +26,33 @@ public class Player : MonoBehaviour
     void Update()
     {
         Movement();
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
     }
 
     private void Movement ()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-        float positionX = transform.position.x;
-        float positionY = transform.position.y;
-        float positionZ = transform.position.z;
+        _positionX = transform.position.x;
+        _positionY = transform.position.y;
+        _positionZ = transform.position.z;
         transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
         transform.Translate(Vector3.up * Time.deltaTime * speed * verticalInput);
-        if (positionX > 8) transform.position = new Vector3(8, positionY, positionZ);
-        if (positionX < -8) transform.position = new Vector3(-8, positionY, positionZ);
-        if (positionY > 4) transform.position = new Vector3(positionX, 4, positionZ);
-        if (positionY < -4) transform.position = new Vector3(positionX, -4, positionZ);
+        if (_positionX > 8) transform.position = new Vector3(8, _positionY, _positionZ);
+        if (_positionX < -8) transform.position = new Vector3(-8, _positionY, _positionZ);
+        if (_positionY > 4) transform.position = new Vector3(_positionX, 4, _positionZ);
+        if (_positionY < -4) transform.position = new Vector3(_positionX, -4, _positionZ);
+    }
+
+    private void Shoot()
+    {
+        if (Time.time - _lastShoot > _fireRate)
+        {
+            Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.88f, 0), Quaternion.identity);
+            _lastShoot = Time.time;
+        }
     }
 }
