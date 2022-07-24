0c3f367f40a6439cc4b7bfef8c2ca9c8125bfc7a
AILENVENTAJA1\Admin <ailen.ventaja@neoris.com>
2022-07-24 20:29:00 -0300

    powerup triple shot

--- Assets/2D Galaxy Assets/Game/Scripts/Player.cs
+++ Assets/2D Galaxy Assets/Game/Scripts/Player.cs
@@ -6,15 +6,19 @@ public class Player : MonoBehaviour
 {
     [SerializeField]
     private GameObject _laserPrefab;
+    [SerializeField]
+    private GameObject _tripleShotPrefab;
     private float _positionX;
     private float _positionY;
     private float _positionZ;
     [SerializeField]
     private float _fireRate = 0.25f;
     private float _lastShoot = 0.0f;
+    [SerializeField]
+    private bool isTripleShot;
 
     [SerializeField]
-    private float speed = 5.0f;
+    private float _speed = 5.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -24,12 +28,12 @@ public class Player : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        Movement();
-        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
-        {
-            Shoot();
-        }
+    {  
+       Movement();
+       if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+       {
+           Shoot();
+       }
     }
 
     private void Movement ()
@@ -39,8 +43,8 @@ public class Player : MonoBehaviour
         _positionX = transform.position.x;
         _positionY = transform.position.y;
         _positionZ = transform.position.z;
-        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
-        transform.Translate(Vector3.up * Time.deltaTime * speed * verticalInput);
+        transform.Translate(Vector3.right * Time.deltaTime * _speed * horizontalInput);
+        transform.Translate(Vector3.up * Time.deltaTime * _speed * verticalInput);
         if (_positionX > 8) transform.position = new Vector3(8, _positionY, _positionZ);
         if (_positionX < -8) transform.position = new Vector3(-8, _positionY, _positionZ);
         if (_positionY > 4) transform.position = new Vector3(_positionX, 4, _positionZ);
@@ -51,8 +55,26 @@ public class Player : MonoBehaviour
     {
         if (Time.time - _lastShoot > _fireRate)
         {
-            Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.88f, 0), Quaternion.identity);
+            if (isTripleShot == true)
+            {
+                Instantiate(_tripleShotPrefab, transform.position + new Vector3(0.2627442f, -0.1591894f, 0), Quaternion.identity);
+            }
+            else
+            {
+                Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.88f, 0), Quaternion.identity);
+            }
             _lastShoot = Time.time;
         }
+    }
+    public void TripleShotTurnOn()
+    {
+        isTripleShot = true;
+        StartCoroutine(TripleShotTurnOff());
+    }
+
+    IEnumerator TripleShotTurnOff()
+    {
+        yield return new WaitForSeconds(10.0f);
+        isTripleShot = false;
     }
 }
